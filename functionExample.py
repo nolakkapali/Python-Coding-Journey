b5a05b069c01ec1308b9830d2ddbb6fdc7caf5a7
nolakkapali <nolak.kapali@gmail.com>
2026-05-15 22:53:23 +0600

    new function exercise is added

--- functionExample.py
+++ functionExample.py
@@ -6,11 +6,16 @@
 
 # myfunc() Ans: x name is not defined
 
-def myfunc():
-  global x
-  x = 300
-def myinnerfunc():
-    print(x)
-myinnerfunc()
+# def myfunc():
+#   global x
+#   x = 300
+# def myinnerfunc():
+#     print(x)
+# myinnerfunc()
 
-myfunc()
+# myfunc()
+
+#function e value chara maan shbar prthom boshbe parameter e ar defalut value soho parameter erpor e boshbe
+def fruits(weight,name="apple", color="red"):#jaar jonno weight age e boshe ekane
+   print("these are",name,"weight=",weight,"color is=",color)
+fruits(23,color="yellow")
