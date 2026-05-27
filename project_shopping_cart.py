ec8e917cbe2ca6e4a75b3f5093f75e0d95fa870d
nolakkapali <nolak.kapali@gmail.com>
2026-05-27 23:49:10 +0600

    created add to cart function in shopping cart

--- project_shopping_cart.py
+++ project_shopping_cart.py
@@ -3,7 +3,12 @@ product_catalog={"product1":{"ID":1234,"name":"book","price":234},
                  "product3":{"ID":2324,"name":"clock","price":3354}
 }
 
-shopping_cart={"ID":11,"Quantity":4,
-               "ID2":46,"Quantity2":45,}
+shopping_cart={"UserID":11,"Quantity":4,
+               "UserID2":46,"Quantity2":45,}
 
-def add_to_cart:
+#add product to cart
+def add_to_cart(productID,Quantity):
+    if productID in product_catalog:
+
+
+add_to_cart(product_catalog["product1"],shopping_cart["Quantity"])
