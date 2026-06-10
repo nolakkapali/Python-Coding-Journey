00fb9328bc906f8c97af951125ff46c7478432c0
nolakkapali <nolak.kapali@gmail.com>
2026-06-10 23:34:05 +0600

    added changing tuples examples

--- tupleexercise.py
+++ tupleexercise.py
@@ -4,7 +4,21 @@ createtuple=tuple((1,2,3,4,"a","bhjbkd",6))
 print(type(createtuple))
 print(len(createtuple))
 print(createtuple)
+#accessing tuple
 print(createtuple[2])
 print(createtuple[4:7])
 print(createtuple[-5:-2])
 print(createtuple[-3:-1])
+#updating tuples
+a=tuple(("a","b","c","d"))
+print(a)
+b=list(a)
+print(type(b))
+b[0]=1
+print(b)
+c=tuple(b)
+print(a)
+print(c)
+#adding tuples
+
+#deleting tuples
